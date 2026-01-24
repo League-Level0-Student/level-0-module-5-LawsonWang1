e6b93888b7bbe98c514c7a1455a761d202231124
league <league@192.168.1.214>
2026-01-24 14:29:57 -0800

    Working on ObedientRobot

--- src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
@@ -6,45 +6,82 @@ public class LoopGauntlet {
  
 	public static void main(String[] args) {
 	
-		for (int x = 1; x < 100; x++) {
-			System.out.print(x + ", ");
-		}   System.out.println(100);
+//		for (int x = 1; x < 100; x++) {
+//			System.out.print(x + ", ");
+//		}   System.out.println(100);
+//		
+//		for (int x = 100; x > 1; x--) {
+//			System.out.print(x + ", ");
+//		}	System.out.println(1);
+//	
+//
+//		for (int x = 2; x < 100; x+=2) {
+//			System.out.print(x + ", ");
+//		}   System.out.println(100);
+//		
+//		for (int x = 1; x < 99; x+=2) {
+//			System.out.print(x + ", ");
+//		}   System.out.println(99);
+//	 
+//		for (int x = 1; x < 101; x++) {
+//			System.out.print(x);
+//			if (x % 2 == 0) {
+//				System.out.println(" is even");
+//			} else {
+//				System.out.println(" is odd");
+//			}
+//		}
+//		for (int x = 0; x < 777; x+=7) {
+//			System.out.print(x + ", ");
+//		}   System.out.println(777);
+//		
+//
+//		for (int x = 1; x < 100; x++) {
+//			System.out.print(x + ", ");
+//		}   System.out.println(100);
+//		
+//		for (int x = 2011; x < 2027; x++) {
+//			System.out.println("In " + x + " I was " + (x-2011) + " years old");
+//			}
+//		
+//		for (int x = 0; x < 3; x++) {
+//			for (int y = 0; y < 3; y++) {
+//				System.out.print(x);
+//				System.out.print(y);
+//				System.out.println();
+//			}
+//		}
 		
-		for (int x = 100; x > 1; x--) {
-			System.out.print(x + ", ");
-		}	System.out.println(1);
-	
-
-		for (int x = 2; x < 100; x+=2) {
-			System.out.print(x + ", ");
-		}   System.out.println(100);
+//		for (int x = 0; x < 9; x+=3) {
+//			for (int y = 1; y < 4; y++) {
+//				System.out.print(y+x);
+//			}
+//			
+//			System.out.println();
+//		}
 		
-		for (int x = 1; x < 99; x+=2) {
-			System.out.print(x + ", ");
-		}   System.out.println(99);
-	 
-		for (int x = 1; x < 101; x++) {
-			System.out.print(x);
-			if (x % 2 == 0) {
-				System.out.println(" is even");
-			} else {
-				System.out.println(" is odd");
+//		for (int x = 0; x <= 9; x++) {
+//			for (int y = 1; y <= 10; y++) {
+//				System.out.print(y+10*x + " ");
+//			}
+//			
+//			System.out.println();
+//		}
+		
+//		for(int i = 2; i < 8; i++) {
+//			for(int x = 1; x < i; x++) {
+//				System.out.print("*");
+//				
+//			}
+//			System.out.println();
+//		}
+		
+		for(int i = 0; i <= 0; i++)
+			for(int x = 100; x > 0; x-=i+1) {
+				System.out.println(x);
 			}
-		}
-		for (int x = 0; x < 777; x+=7) {
-			System.out.print(x + ", ");
-		}   System.out.println(777);
 		
-
-		for (int x = 1; x < 100; x++) {
-			System.out.print(x + ", ");
-		}   System.out.println(100);
 		
-		for (int x = 2011; x < 2027; x++) {
-			System.out.println("In " + x + " I was " + (x-2011) + " years old");
-			}
-		
-	
 		
 		
 		
--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,12 +1,67 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class ObedientRobot {
 
 	public static void main(String[] args) {
 		
 		
+		Robot rob = new Robot();
+		rob.penDown();
+		String shape = JOptionPane.showInputDialog("Which shape do you want me to draw?");
+		if(shape.equalsIgnoreCase("square")) {
+			drawSquare();
+		}else if(shape.equalsIgnoreCase("triangle")) {
+			drawTriangle();
+		}else if(shape.equalsIgnoreCase("circle")) {
+			drawCircle();
+		}
+		String color = JOptionPane.showInputDialog("What color do you want the shape to be?");
+		if(color.equalsIgnoreCase("red")) {
+			rob.setPenColor(255,0,0);
+		}else if(color.equalsIgnoreCase("yellow")) {
+			rob.setPenColor(255,255,0);
+		}else if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(0,0, 255);
+		}
 		
 		
 		
-	}
+		
+		}
+		
+		public static void drawSquare() {
+			for(int i = 0; i < 4; i++) {
+				Robot rob = new Robot();
+				rob.penDown();
+				rob.move(50);
+				rob.turn(90);
+			}
+		}
+		public static void drawTriangle() {
+		for(int i = 0; i < 3; i++) {
+			Robot rob = new Robot();
+			rob.penDown();
+			rob.move(50);
+			rob.turn(120);
+			
+			}
+		}
+		public static void drawCircle() {
+			for(int i = 0; i < 3; i++) {
+				
+				}
+			}
+		
+		
+		
+		
+		
 }
+		
+		
+		
+		
