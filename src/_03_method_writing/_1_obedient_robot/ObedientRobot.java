aea69c4549ae0fd0d0afaf0aa47c982dfa37c754
league <league@192.168.1.214>
2026-02-01 11:29:28 -0800

    Finish HappyPet

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -5,20 +5,14 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
+	
+	static Robot rob = new Robot();
+	
 
 	public static void main(String[] args) {
 		
 		
-		Robot rob = new Robot();
-		rob.penDown();
-		String shape = JOptionPane.showInputDialog("Which shape do you want me to draw?");
-		if(shape.equalsIgnoreCase("square")) {
-			drawSquare();
-		}else if(shape.equalsIgnoreCase("triangle")) {
-			drawTriangle();
-		}else if(shape.equalsIgnoreCase("circle")) {
-			drawCircle();
-		}
+		
 		String color = JOptionPane.showInputDialog("What color do you want the shape to be?");
 		if(color.equalsIgnoreCase("red")) {
 			rob.setPenColor(255,0,0);
@@ -27,15 +21,19 @@ public class ObedientRobot {
 		}else if(color.equalsIgnoreCase("blue")) {
 			rob.setPenColor(0,0, 255);
 		}
-		
-		
+		String shape = JOptionPane.showInputDialog("Which shape do you want me to draw?");
+		if(shape.equalsIgnoreCase("square")) {
+			drawSquare();
+		}else if(shape.equalsIgnoreCase("triangle")) {
+			drawTriangle();
+		}else if(shape.equalsIgnoreCase("circle")) {
+			drawCircle();
+		}
 		
 		
 		}
-		
 		public static void drawSquare() {
 			for(int i = 0; i < 4; i++) {
-				Robot rob = new Robot();
 				rob.penDown();
 				rob.move(50);
 				rob.turn(90);
@@ -43,7 +41,7 @@ public class ObedientRobot {
 		}
 		public static void drawTriangle() {
 		for(int i = 0; i < 3; i++) {
-			Robot rob = new Robot();
+			
 			rob.penDown();
 			rob.move(50);
 			rob.turn(120);
@@ -51,8 +49,10 @@ public class ObedientRobot {
 			}
 		}
 		public static void drawCircle() {
-			for(int i = 0; i < 3; i++) {
-				
+			for(int i = 0; i < 360; i++) {
+				rob.penDown();
+				rob.move(1);
+				rob.turn(1);
 				}
 			}
 		
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,31 +1,90 @@
 package _03_method_writing._2_happy_pet;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 1. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 1. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
 	static int happinessLevel = 0;
+
+	static String pet = JOptionPane.showInputDialog("Which one of these pets do you want to buy: dog, cat, or fish?");
+
 	public static void main(String[] args) {
-		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-		String pet = JOptionPane.showInputDialog("What pet do you want to buy?");
-			// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+		// 2. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
 		
-			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			String happy = JOptionPane.showInputDialog("What do you want to do make your pet happy?")
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 
-			// 6. Use user input to call the appropriate method created in step 5 below.
-
-			// 7. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
-
+		// 4. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+		int petTask = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?",
+				"Interactions", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Feed the pet", "Take the pet on a walk", "Play with the pet" }, null);
+		// 6. Use user input to call the appropriate method created in step 5 below.
+		switch (petTask) {
+		case 0:
+			food();
+			break;
+		case 1:
+			walk();
+			break;
+		case 2:
+			play();
+			break;
+		}
+		// 7. If you determine the happiness level is large enough, tell the
+		// user that he loves his pet and use break; to exit for loop.
+		if (happinessLevel >= 10) {
+			System.exit(0);
+		}
 	}
 
 	// 5. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+
+	public static void food() {
+		if (pet.equalsIgnoreCase("dog")) {
+			JOptionPane.showMessageDialog(null, "The dog wagged its tail");
+			happinessLevel += 2;
+		} else if (pet.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "The cat purred");
+			happinessLevel += 2;
+		} else if (pet.equalsIgnoreCase("fish")) {
+			happinessLevel +=2;
+		}
+	}
+
+	public static void walk() {
+		if (pet.equalsIgnoreCase("dog")) {
+			JOptionPane.showMessageDialog(null, "The dog felt happy");
+			happinessLevel += 1;
+		} else if (pet.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "The cat got tired");
+			happinessLevel -= 1;
+		} else if (pet.equalsIgnoreCase("fish")) {
+			JOptionPane.showMessageDialog(null, "The fish needs to live in water");
+			happinessLevel -= 3;
+		}
+		
+	}
+
+	public static void play() {
+		if (pet.equalsIgnoreCase("dog")) {
+			JOptionPane.showMessageDialog(null, "The dog was very interested");
+			happinessLevel += 1;
+		} else if (pet.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "The cat was interested for a few minutes");
+			happinessLevel += 1;
+		} else if (pet.equalsIgnoreCase("fish")) {
+			JOptionPane.showMessageDialog(null, "The fish did not pay attention to you");
+			happinessLevel +=0;
+		}
+	}
+
 }
